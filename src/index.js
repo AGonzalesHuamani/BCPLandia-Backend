252d817f721830b5281a2b2580ce2c52d0fa0dbd
Antone Gonzales <es.angelika@gmail.com>
2025-03-27 01:21:21 -0500

    feat: file was created index.js router and express configuration.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const router = require("./routes");
 const app = express();
 
-const PORT = process.env.PORT || 8000;
+app.use(express.json());
+app.use(router);
+
+const PORT = 8000;
 
 app.get("/", (req, res) => {
     res.send("Hello World!");});

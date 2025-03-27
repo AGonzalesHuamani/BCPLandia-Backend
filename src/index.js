0468faac09b33c33db6ae723d941f38686b73e7f
Antone Gonzales <es.angelika@gmail.com>
2025-03-26 21:07:36 -0500

    feat: add index.js to start server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("first")
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});

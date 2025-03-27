a6047b1ab733a28bf33db7f7bb87e5e6f53c4d42
Antone Gonzales <es.angelika@gmail.com>
2025-03-27 13:13:48 -0500

    feat: add endpoint to get mini-stories by storyId

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -3,6 +3,20 @@ const userController = {
   getUsers: (req, res) => {
     res.json(data.users);
   },
+  getMiniStoriesByStoryId: (req, res) => {
+    const { storyId } = req.params; // Obtener el parámetro de la URL
+    const storyIdNumber = parseInt(storyId, 10); // Convertir a número
+
+    // Filtrar las minihistorias que coincidan con el storyId
+    const miniStories = data.miniStories.filter(miniStory => miniStory.storyId === storyIdNumber);
+
+    // Verificar si se encontraron minihistorias
+    if (miniStories.length === 0) {
+      return res.status(404).json({ message: `No se encontraron minihistorias para storyId ${storyId}` });
+    }
+
+    res.json(miniStories);
+  },
 };
 
 module.exports = userController;
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 app.use(router);
 
-const PORT = 8000;
+const PORT = 8080;
 
 app.get("/", (req, res) => {
     res.send("Hello World!");});

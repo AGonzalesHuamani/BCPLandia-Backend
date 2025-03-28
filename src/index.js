42745b83e40460632d29954441f8c955a07512fc
Aylin <aylin_santacruz@hotmail.com>
2025-03-27 20:24:52 -0500

    feat:add port and cors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = require("./routes");
+const cors = require("cors")
+
 
 const app = express();
+
+app.use(cors())
 app.use(express.json());
 app.use(router);
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
     res.send("Hello World!");});
